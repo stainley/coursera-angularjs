da3cc9f6e0a28ef77a6ea75e9a996c0797b4b719
Stainley Lebron <saiyamans@gmail.com>
2016-09-18 19:20:43 -0400

    Soluction completed!

--- js/app.js
+++ js/app.js
@@ -7,21 +7,32 @@
   LunchCheckController.$inject = ['$scope'];
 
   function LunchCheckController($scope){
-    $scope.name = "Stainley";
-
-    $scope.upper = function(){
-      var upCase = $filter('uppercase');
-      $scope.name = upCase($scope.name);
-    };
 
     $scope.message = "";
+    $scope.items = "";
 
-    $scope.sayHello = function(){
-      $scope.message = "I like to Eaet";
-      return "I like to Eat";
+    $scope.calculate = function(){
+      $scope.message = calculateItem($scope.items);
     };
 
+    function calculateItem(string){
+      var total = string.split(',');
+      var text = "";
+
+      if(string == ""){
+        return "Please enter data first";
+      } else {
+        var x = 0;
+        for (var a in total) {
+          x += 1;
+        }
+        if(x >= 1 && x <= 3){
+          text = "Enjoy!"
+        }else if (x >= 4) {
+          text = "Too much!";
+        }
+      }
+      return text;
+    };
   };
-
-
 })();
